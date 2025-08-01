20425eb46a97317d1e88840436ff38d7968fbdfc
Allison Blum <118923616+alliblum@users.noreply.github.com>
2025-08-01 16:50:07 -0400

    adding sanity studio link

--- frontend/sanity/lib/api.ts
+++ frontend/sanity/lib/api.ts
@@ -13,12 +13,12 @@ function assertValue<T>(v: T | undefined, errorMessage: string): T {
 
 export const dataset = assertValue(
   process.env.NEXT_PUBLIC_SANITY_DATASET,
-  "Missing environment variable: NEXT_PUBLIC_SANITY_DATASET",
+  "Missing environment variable: NEXT_PUBLIC_SANITY_DATASET"
 );
 
 export const projectId = assertValue(
   process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  "Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID",
+  "Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID"
 );
 
 /**
@@ -31,4 +31,5 @@ export const apiVersion =
  * Used to configure edit intent links, for Presentation Mode, as well as to configure where the Studio is mounted in the router.
  */
 export const studioUrl =
-  process.env.NEXT_PUBLIC_SANITY_STUDIO_URL || "http://localhost:3333";
+  // process.env.NEXT_PUBLIC_SANITY_STUDIO_URL || "http://localhost:3333";
+  process.env.NEXT_PUBLIC_SANITY_STUDIO_URL || "http://hd.sanity.studio";
